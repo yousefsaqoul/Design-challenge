57d1bde192f128567146df11921e3c899678f338
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-01-22 01:28:05 +0100

    Design-challenge

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+
+function myFunction(){
+
+    document.getElementById('comfirmMessege').style.display = 'block';
+    document.getElementById('formHidden').style.display = 'none';
+}
+ function back(){
+    document.getElementById('formHidden').style.display = 'block';
+    document.getElementById('comfirmMessege').style.display = 'none';
+
+
+ }
